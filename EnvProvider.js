dfae2fad4c2b8f5a093651c9d184e84cbfa14036
Juan Nicolau Sepúlveda <jnicolau@iti.es>
2020-11-18 20:18:06 +0100

    Modfied the way we export the library, and added support to change public path

--- EnvProvider.js
+++ EnvProvider.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+var dotenv = require('dotenv');
 
 dotenv.config();
 
@@ -11,10 +11,28 @@ class EnvProvider {
   }
 
   static value(name) {
-    // console.log('Se busca la clave', name);
+    //console.log('Searching key', name);
+    // if (!name.startsWith('JVJR_CONFIG_') && !(name in this.envVars)) {
+    //   //console.log('Not found', name);
+    //   return;
+    // }
     if (!(name in this.envVars)) {
       return;
     }
+
+
+    // let value = '';
+    //
+    // if (name.startsWith('JVJR_CONFIG_')) {
+    //   if (process.env.NODE_ENV === 'development') {
+    //     name = name.replace('JVJR_CONFIG_', '');
+    //     value = this.envVars[name];
+    //   } else {
+    //     return '#' + name;
+    //   }
+    // } else {
+    //   value = this.envVars[name];
+    // }
 
     const value = this.envVars[name];
     // console.log('El valor de la clave es:', value);
@@ -25,17 +43,17 @@ class EnvProvider {
     if (value.startsWith('$VUE_APP_') || value.startsWith('$REACT_APP_')) {
       const envName = value.substr(1);
       const envValue = process.env[envName];
-      console.log('Valor de entorno devuelto', envValue);
+      // console.log('Valor de entorno devuelto', envValue);
       if (envValue) {
         return envValue;
       } else {
         return;
       }
     } else {
-      console.log('valor introducido', value);
+      // console.log('valor introducido', value);
       return value;
     }
   }
 }
 
-export default EnvProvider;
+module.exports = EnvProvider;
