6ee59373a804bf0ab9429424d3b8f00d6f381859
jajimgme <jajimgme@users.noreply.github.com>
2020-11-19 07:53:39 +0100

    Fix path to be compatible with windows installations. (#2)
    Co-authored-by: jjimenez-iti <jjimenez@iti.es>

--- scripts/start.js
+++ scripts/start.js
@@ -1,7 +1,6 @@
 const { processScript } = require('../src/main');
-
 processScript({
     command: 'start',
     template: 'jvjr',
-    targetDirectory: process.mainModule.paths[0].split('node_modules')[0].slice(0, -1)
+    targetDirectory: require.main.paths[0].split('node_modules')[0].slice(0, -1)
 });
